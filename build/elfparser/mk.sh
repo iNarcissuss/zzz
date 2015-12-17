13cb39e45e090c48a63949cb70b892a9c80fdf06
vdun <vdun@users.noreply.github.com>
2015-12-18 00:43:13 +0100

    Update mk.sh

--- build/elfparser/mk.sh
+++ build/elfparser/mk.sh
@@ -1,3 +1,5 @@
+echo "deb http://repo.kali.org/kali sana main non-free contrib" > /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED444FF07D8D0BF6
 apt-get update
 apt-get install cmake
 mkdir build
