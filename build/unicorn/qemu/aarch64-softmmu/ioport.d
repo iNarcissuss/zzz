7373d1acb755c283d107e439b441d310a1b5af87
Drone Build <support@drone.io>
2015-12-18 00:54:34 +0000

    unicorn_2015-12-18-00:54

---
+++ build/unicorn/qemu/aarch64-softmmu/ioport.d
@@ -0,0 +1,403 @@
+ioport.o: /tmp/unicorn/qemu/ioport.c /tmp/unicorn/qemu/aarch64.h \
+ /tmp/unicorn/qemu/include/exec/ioport.h \
+ /tmp/unicorn/qemu/include/qemu-common.h \
+ /tmp/unicorn/qemu/include/qemu/compiler.h \
+ /tmp/unicorn/qemu/config-host.h \
+ /tmp/unicorn/qemu/include/qemu/typedefs.h \
+ /tmp/unicorn/qemu/include/exec/cpu-common.h \
+ /tmp/unicorn/qemu/include/exec/hwaddr.h \
+ /tmp/unicorn/qemu/include/qemu/bswap.h \
+ /tmp/unicorn/qemu/include/fpu/softfloat.h \
+ /tmp/unicorn/qemu/include/qemu/osdep.h \
+ /tmp/unicorn/qemu/include/qemu/queue.h \
+ /tmp/unicorn/qemu/include/qemu/atomic.h \
+ /tmp/unicorn/qemu/include/qemu/compiler.h \
+ /tmp/unicorn/qemu/include/glib-compat.h /usr/include/glib-2.0/glib.h \
+ /usr/include/glib-2.0/glib/galloca.h /usr/include/glib-2.0/glib/gtypes.h \
+ /usr/lib/x86_64-linux-gnu/glib-2.0/include/glibconfig.h \
+ /usr/include/glib-2.0/glib/gmacros.h \
+ /usr/include/glib-2.0/glib/gversionmacros.h \
+ /usr/include/glib-2.0/glib/garray.h \
+ /usr/include/glib-2.0/glib/gasyncqueue.h \
+ /usr/include/glib-2.0/glib/gthread.h \
+ /usr/include/glib-2.0/glib/gatomic.h /usr/include/glib-2.0/glib/gerror.h \
+ /usr/include/glib-2.0/glib/gquark.h \
+ /usr/include/glib-2.0/glib/gbacktrace.h \
+ /usr/include/glib-2.0/glib/gbase64.h \
+ /usr/include/glib-2.0/glib/gbitlock.h \
+ /usr/include/glib-2.0/glib/gbookmarkfile.h \
+ /usr/include/glib-2.0/glib/gbytes.h \
+ /usr/include/glib-2.0/glib/gcharset.h \
+ /usr/include/glib-2.0/glib/gchecksum.h \
+ /usr/include/glib-2.0/glib/gconvert.h \
+ /usr/include/glib-2.0/glib/gdataset.h /usr/include/glib-2.0/glib/gdate.h \
+ /usr/include/glib-2.0/glib/gdatetime.h \
+ /usr/include/glib-2.0/glib/gtimezone.h /usr/include/glib-2.0/glib/gdir.h \
+ /usr/include/glib-2.0/glib/genviron.h \
+ /usr/include/glib-2.0/glib/gfileutils.h \
+ /usr/include/glib-2.0/glib/ggettext.h /usr/include/glib-2.0/glib/ghash.h \
+ /usr/include/glib-2.0/glib/glist.h /usr/include/glib-2.0/glib/gmem.h \
+ /usr/include/glib-2.0/glib/ghmac.h \
+ /usr/include/glib-2.0/glib/gchecksum.h \
+ /usr/include/glib-2.0/glib/ghook.h \
+ /usr/include/glib-2.0/glib/ghostutils.h \
+ /usr/include/glib-2.0/glib/giochannel.h \
+ /usr/include/glib-2.0/glib/gmain.h /usr/include/glib-2.0/glib/gpoll.h \
+ /usr/include/glib-2.0/glib/gslist.h /usr/include/glib-2.0/glib/gstring.h \
+ /usr/include/glib-2.0/glib/gunicode.h \
+ /usr/include/glib-2.0/glib/gutils.h \
+ /usr/include/glib-2.0/glib/gkeyfile.h \
+ /usr/include/glib-2.0/glib/gmappedfile.h \
+ /usr/include/glib-2.0/glib/gmarkup.h \
+ /usr/include/glib-2.0/glib/gmessages.h \
+ /usr/include/glib-2.0/glib/gnode.h /usr/include/glib-2.0/glib/goption.h \
+ /usr/include/glib-2.0/glib/gpattern.h \
+ /usr/include/glib-2.0/glib/gprimes.h /usr/include/glib-2.0/glib/gqsort.h \
+ /usr/include/glib-2.0/glib/gqueue.h /usr/include/glib-2.0/glib/grand.h \
+ /usr/include/glib-2.0/glib/gregex.h \
+ /usr/include/glib-2.0/glib/gscanner.h \
+ /usr/include/glib-2.0/glib/gsequence.h \
+ /usr/include/glib-2.0/glib/gshell.h /usr/include/glib-2.0/glib/gslice.h \
+ /usr/include/glib-2.0/glib/gspawn.h \
+ /usr/include/glib-2.0/glib/gstrfuncs.h \
+ /usr/include/glib-2.0/glib/gstringchunk.h \
+ /usr/include/glib-2.0/glib/gtestutils.h \
+ /usr/include/glib-2.0/glib/gthreadpool.h \
+ /usr/include/glib-2.0/glib/gtimer.h \
+ /usr/include/glib-2.0/glib/gtrashstack.h \
+ /usr/include/glib-2.0/glib/gtree.h \
+ /usr/include/glib-2.0/glib/gurifuncs.h \
+ /usr/include/glib-2.0/glib/gvarianttype.h \
+ /usr/include/glib-2.0/glib/gvariant.h \
+ /usr/include/glib-2.0/glib/gversion.h \
+ /usr/include/glib-2.0/glib/deprecated/gallocator.h \
+ /usr/include/glib-2.0/glib/deprecated/gcache.h \
+ /usr/include/glib-2.0/glib/deprecated/gcompletion.h \
+ /usr/include/glib-2.0/glib/deprecated/gmain.h \
+ /usr/include/glib-2.0/glib/deprecated/grel.h \
+ /usr/include/glib-2.0/glib/deprecated/gthread.h \
+ /tmp/unicorn/qemu/include/qemu/osdep.h \
+ /tmp/unicorn/qemu/include/qemu/bswap.h \
+ /tmp/unicorn/qemu/target-arm/cpu.h /tmp/unicorn/qemu/include/config.h \
+ config-target.h /tmp/unicorn/qemu/target-arm/kvm-consts.h \
+ /tmp/unicorn/qemu/include/exec/cpu-defs.h \
+ /tmp/unicorn/qemu/target-arm/cpu-qom.h \
+ /tmp/unicorn/qemu/include/qom/cpu.h \
+ /tmp/unicorn/qemu/include/hw/qdev-core.h \
+ /tmp/unicorn/qemu/include/qemu/typedefs.h \
+ /tmp/unicorn/qemu/include/qemu/bitmap.h \
+ /tmp/unicorn/qemu/include/qemu/bitops.h \
+ /tmp/unicorn/qemu/include/qemu/host-utils.h \
+ /tmp/unicorn/qemu/include/qom/object.h \
+ /tmp/unicorn/qemu/include/qapi/error.h /tmp/unicorn/qemu/qapi-types.h \
+ /tmp/unicorn/qemu/include/hw/irq.h \
+ /tmp/unicorn/qemu/include/qemu/thread.h \
+ /tmp/unicorn/qemu/include/qemu/thread-posix.h \
+ /tmp/unicorn/qemu/include/qemu/tls.h \
+ /tmp/unicorn/qemu/include/exec/cpu-all.h \
+ /tmp/unicorn/qemu/include/exec/cpu-common.h \
+ /tmp/unicorn/qemu/include/exec/memory.h \
+ /tmp/unicorn/qemu/include/qemu/int128.h \
+ /tmp/unicorn/qemu/include/qemu/notify.h \
+ /tmp/unicorn/qemu/include/exec/exec-all.h \
+ /tmp/unicorn/qemu/include/qemu/log.h \
+ /tmp/unicorn/qemu/include/exec/spinlock.h \
+ /tmp/unicorn/qemu/include/qemu/module.h \
+ /tmp/unicorn/qemu/include/exec/address-spaces.h \
+ /tmp/unicorn/qemu/../include/uc_priv.h \
+ /tmp/unicorn/qemu/../include/qemu.h \
+ /tmp/unicorn/qemu/include/sysemu/sysemu.h \
+ /tmp/unicorn/qemu/include/qemu/main-loop.h \
+ /tmp/unicorn/qemu/include/qemu/timer.h \
+ /tmp/unicorn/qemu/include/sysemu/cpus.h \
+ /tmp/unicorn/qemu/include/qom/cpu.h /tmp/unicorn/qemu/vl.h \
+ /tmp/unicorn/qemu/../include/unicorn/unicorn.h \
+ /tmp/unicorn/qemu/../include/unicorn/platform.h \
+ /tmp/unicorn/qemu/../include/unicorn/m68k.h \
+ /tmp/unicorn/qemu/../include/unicorn/x86.h \
+ /tmp/unicorn/qemu/../include/unicorn/arm.h \
+ /tmp/unicorn/qemu/../include/unicorn/arm64.h \
+ /tmp/unicorn/qemu/../include/unicorn/mips.h \
+ /tmp/unicorn/qemu/../include/unicorn/sparc.h \
+ /tmp/unicorn/qemu/../include/hook.h \
+ /tmp/unicorn/qemu/../include/qemu_macro.h
+
+/tmp/unicorn/qemu/aarch64.h:
+
+/tmp/unicorn/qemu/include/exec/ioport.h:
+
+/tmp/unicorn/qemu/include/qemu-common.h:
+
+/tmp/unicorn/qemu/include/qemu/compiler.h:
+
+/tmp/unicorn/qemu/config-host.h:
+
+/tmp/unicorn/qemu/include/qemu/typedefs.h:
+
+/tmp/unicorn/qemu/include/exec/cpu-common.h:
+
+/tmp/unicorn/qemu/include/exec/hwaddr.h:
+
+/tmp/unicorn/qemu/include/qemu/bswap.h:
+
+/tmp/unicorn/qemu/include/fpu/softfloat.h:
+
+/tmp/unicorn/qemu/include/qemu/osdep.h:
+
+/tmp/unicorn/qemu/include/qemu/queue.h:
+
+/tmp/unicorn/qemu/include/qemu/atomic.h:
+
+/tmp/unicorn/qemu/include/qemu/compiler.h:
+
+/tmp/unicorn/qemu/include/glib-compat.h:
+
+/usr/include/glib-2.0/glib.h:
+
+/usr/include/glib-2.0/glib/galloca.h:
+
+/usr/include/glib-2.0/glib/gtypes.h:
+
+/usr/lib/x86_64-linux-gnu/glib-2.0/include/glibconfig.h:
+
+/usr/include/glib-2.0/glib/gmacros.h:
+
+/usr/include/glib-2.0/glib/gversionmacros.h:
+
+/usr/include/glib-2.0/glib/garray.h:
+
+/usr/include/glib-2.0/glib/gasyncqueue.h:
+
+/usr/include/glib-2.0/glib/gthread.h:
+
+/usr/include/glib-2.0/glib/gatomic.h:
+
+/usr/include/glib-2.0/glib/gerror.h:
+
+/usr/include/glib-2.0/glib/gquark.h:
+
+/usr/include/glib-2.0/glib/gbacktrace.h:
+
+/usr/include/glib-2.0/glib/gbase64.h:
+
+/usr/include/glib-2.0/glib/gbitlock.h:
+
+/usr/include/glib-2.0/glib/gbookmarkfile.h:
+
+/usr/include/glib-2.0/glib/gbytes.h:
+
+/usr/include/glib-2.0/glib/gcharset.h:
+
+/usr/include/glib-2.0/glib/gchecksum.h:
+
+/usr/include/glib-2.0/glib/gconvert.h:
+
+/usr/include/glib-2.0/glib/gdataset.h:
+
+/usr/include/glib-2.0/glib/gdate.h:
+
+/usr/include/glib-2.0/glib/gdatetime.h:
+
+/usr/include/glib-2.0/glib/gtimezone.h:
+
+/usr/include/glib-2.0/glib/gdir.h:
+
+/usr/include/glib-2.0/glib/genviron.h:
+
+/usr/include/glib-2.0/glib/gfileutils.h:
+
+/usr/include/glib-2.0/glib/ggettext.h:
+
+/usr/include/glib-2.0/glib/ghash.h:
+
+/usr/include/glib-2.0/glib/glist.h:
+
+/usr/include/glib-2.0/glib/gmem.h:
+
+/usr/include/glib-2.0/glib/ghmac.h:
+
+/usr/include/glib-2.0/glib/gchecksum.h:
+
+/usr/include/glib-2.0/glib/ghook.h:
+
+/usr/include/glib-2.0/glib/ghostutils.h:
+
+/usr/include/glib-2.0/glib/giochannel.h:
+
+/usr/include/glib-2.0/glib/gmain.h:
+
+/usr/include/glib-2.0/glib/gpoll.h:
+
+/usr/include/glib-2.0/glib/gslist.h:
+
+/usr/include/glib-2.0/glib/gstring.h:
+
+/usr/include/glib-2.0/glib/gunicode.h:
+
+/usr/include/glib-2.0/glib/gutils.h:
+
+/usr/include/glib-2.0/glib/gkeyfile.h:
+
+/usr/include/glib-2.0/glib/gmappedfile.h:
+
+/usr/include/glib-2.0/glib/gmarkup.h:
+
+/usr/include/glib-2.0/glib/gmessages.h:
+
+/usr/include/glib-2.0/glib/gnode.h:
+
+/usr/include/glib-2.0/glib/goption.h:
+
+/usr/include/glib-2.0/glib/gpattern.h:
+
+/usr/include/glib-2.0/glib/gprimes.h:
+
+/usr/include/glib-2.0/glib/gqsort.h:
+
+/usr/include/glib-2.0/glib/gqueue.h:
+
+/usr/include/glib-2.0/glib/grand.h:
+
+/usr/include/glib-2.0/glib/gregex.h:
+
+/usr/include/glib-2.0/glib/gscanner.h:
+
+/usr/include/glib-2.0/glib/gsequence.h:
+
+/usr/include/glib-2.0/glib/gshell.h:
+
+/usr/include/glib-2.0/glib/gslice.h:
+
+/usr/include/glib-2.0/glib/gspawn.h:
+
+/usr/include/glib-2.0/glib/gstrfuncs.h:
+
+/usr/include/glib-2.0/glib/gstringchunk.h:
+
+/usr/include/glib-2.0/glib/gtestutils.h:
+
+/usr/include/glib-2.0/glib/gthreadpool.h:
+
+/usr/include/glib-2.0/glib/gtimer.h:
+
+/usr/include/glib-2.0/glib/gtrashstack.h:
+
+/usr/include/glib-2.0/glib/gtree.h:
+
+/usr/include/glib-2.0/glib/gurifuncs.h:
+
+/usr/include/glib-2.0/glib/gvarianttype.h:
+
+/usr/include/glib-2.0/glib/gvariant.h:
+
+/usr/include/glib-2.0/glib/gversion.h:
+
+/usr/include/glib-2.0/glib/deprecated/gallocator.h:
+
+/usr/include/glib-2.0/glib/deprecated/gcache.h:
+
+/usr/include/glib-2.0/glib/deprecated/gcompletion.h:
+
+/usr/include/glib-2.0/glib/deprecated/gmain.h:
+
+/usr/include/glib-2.0/glib/deprecated/grel.h:
+
+/usr/include/glib-2.0/glib/deprecated/gthread.h:
+
+/tmp/unicorn/qemu/include/qemu/osdep.h:
+
+/tmp/unicorn/qemu/include/qemu/bswap.h:
+
+/tmp/unicorn/qemu/target-arm/cpu.h:
+
+/tmp/unicorn/qemu/include/config.h:
+
+config-target.h:
+
+/tmp/unicorn/qemu/target-arm/kvm-consts.h:
+
+/tmp/unicorn/qemu/include/exec/cpu-defs.h:
+
+/tmp/unicorn/qemu/target-arm/cpu-qom.h:
+
+/tmp/unicorn/qemu/include/qom/cpu.h:
+
+/tmp/unicorn/qemu/include/hw/qdev-core.h:
+
+/tmp/unicorn/qemu/include/qemu/typedefs.h:
+
+/tmp/unicorn/qemu/include/qemu/bitmap.h:
+
+/tmp/unicorn/qemu/include/qemu/bitops.h:
+
+/tmp/unicorn/qemu/include/qemu/host-utils.h:
+
+/tmp/unicorn/qemu/include/qom/object.h:
+
+/tmp/unicorn/qemu/include/qapi/error.h:
+
+/tmp/unicorn/qemu/qapi-types.h:
+
+/tmp/unicorn/qemu/include/hw/irq.h:
+
+/tmp/unicorn/qemu/include/qemu/thread.h:
+
+/tmp/unicorn/qemu/include/qemu/thread-posix.h:
+
+/tmp/unicorn/qemu/include/qemu/tls.h:
+
+/tmp/unicorn/qemu/include/exec/cpu-all.h:
+
+/tmp/unicorn/qemu/include/exec/cpu-common.h:
+
+/tmp/unicorn/qemu/include/exec/memory.h:
+
+/tmp/unicorn/qemu/include/qemu/int128.h:
+
+/tmp/unicorn/qemu/include/qemu/notify.h:
+
+/tmp/unicorn/qemu/include/exec/exec-all.h:
+
+/tmp/unicorn/qemu/include/qemu/log.h:
+
+/tmp/unicorn/qemu/include/exec/spinlock.h:
+
+/tmp/unicorn/qemu/include/qemu/module.h:
+
+/tmp/unicorn/qemu/include/exec/address-spaces.h:
+
+/tmp/unicorn/qemu/../include/uc_priv.h:
+
+/tmp/unicorn/qemu/../include/qemu.h:
+
+/tmp/unicorn/qemu/include/sysemu/sysemu.h:
+
+/tmp/unicorn/qemu/include/qemu/main-loop.h:
+
+/tmp/unicorn/qemu/include/qemu/timer.h:
+
+/tmp/unicorn/qemu/include/sysemu/cpus.h:
+
+/tmp/unicorn/qemu/include/qom/cpu.h:
+
+/tmp/unicorn/qemu/vl.h:
+
+/tmp/unicorn/qemu/../include/unicorn/unicorn.h:
+
+/tmp/unicorn/qemu/../include/unicorn/platform.h:
+
+/tmp/unicorn/qemu/../include/unicorn/m68k.h:
+
+/tmp/unicorn/qemu/../include/unicorn/x86.h:
+
+/tmp/unicorn/qemu/../include/unicorn/arm.h:
+
+/tmp/unicorn/qemu/../include/unicorn/arm64.h:
+
+/tmp/unicorn/qemu/../include/unicorn/mips.h:
+
+/tmp/unicorn/qemu/../include/unicorn/sparc.h:
+
+/tmp/unicorn/qemu/../include/hook.h:
+
+/tmp/unicorn/qemu/../include/qemu_macro.h:
