0739f576d0839f2fcc441e21652aa506cf216662
vdun <vdun@users.noreply.github.com>
2015-12-18 01:07:26 +0100

    Update mk.sh

--- build/elfparser/mk.sh
+++ build/elfparser/mk.sh
@@ -1,7 +1,13 @@
 echo "deb http://repo.kali.org/kali sana main non-free contrib" > /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED444FF07D8D0BF6
 apt-get update
-apt-get install cmake
+sudo update-alternatives --remove-all gcc 
+sudo update-alternatives --remove-all g++
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
+sudo update-alternatives --config gcc
+sudo update-alternatives --config g++
+apt-get install libc6-dev-i386 g++-4.8-multilib cmake
 mkdir build
 cd build/
 cmake ..
