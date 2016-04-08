891ef3ae8d69cb88a41f73e8fc95d718f1aaa9fa
vdun <vdun@users.noreply.github.com>
2016-04-09 00:34:26 +0200

    Update mk.sh

--- build/unicorn/mk.sh
+++ build/unicorn/mk.sh
@@ -1,9 +1,3 @@
-# sudo update-alternatives --remove-all gcc 
-# sudo update-alternatives --remove-all g++
-# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
-# sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
-# sudo update-alternatives --config gcc
-# sudo update-alternatives --config g++
 apt-get install libglib2.0-dev
 #UNICORN_ARCHS="aarch64" ./make.sh
 ./make.sh
