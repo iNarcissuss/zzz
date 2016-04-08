0e1461898053130bef41b12aae6a7be7ca25b736
vdun <vdun@users.noreply.github.com>
2016-04-08 14:45:55 +0200

    Update mk.sh

--- build/fcd/mk.sh
+++ build/fcd/mk.sh
@@ -1,4 +1,4 @@
-#sed -i 's|trusty|devel|gi' /etc/apt/sources.list
+sed -i 's|trusty|devel|gi' /etc/apt/sources.list
 apt update
 #apt install clang-3.7 llvm-3.7
 apt install clang-3.8 llvm-3.8 libcapstone3 libcapstone-dev libedit-dev python-dev
