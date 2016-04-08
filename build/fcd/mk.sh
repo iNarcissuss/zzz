1208c8279508ba39b8ab72994850d3255ac659a4
vdun <vdun@users.noreply.github.com>
2016-04-08 19:35:34 +0200

    Update mk.sh

--- build/fcd/mk.sh
+++ build/fcd/mk.sh
@@ -1,5 +1,5 @@
 sed -i 's|trusty|devel|gi' /etc/apt/sources.list
 apt update
 #apt install clang-3.7 llvm-3.7
-apt install clang-3.8 llvm-3.8 libcapstone3 libcapstone-dev libedit-dev python-dev
+apt -y install clang-3.8 llvm-3.8 libcapstone3 libcapstone-dev libedit-dev python-dev
 sed -i 's|3.7|3.8|gi' Makefile
