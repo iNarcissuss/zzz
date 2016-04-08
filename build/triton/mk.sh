c376789b1eddb4ba5afbc97a5a2e3df428343dcb
vdun <vdun@users.noreply.github.com>
2016-04-09 00:48:59 +0200

    Update mk.sh

--- build/triton/mk.sh
+++ build/triton/mk.sh
@@ -1,13 +1,13 @@
-set -x
-echo "deb http://repo.kali.org/kali sana main non-free contrib" > /etc/apt/sources.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED444FF07D8D0BF6
-apt-get update
-sudo update-alternatives --remove-all gcc 
-sudo update-alternatives --remove-all g++
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
-sudo update-alternatives --config gcc
-sudo update-alternatives --config g++
+# set -x
+# echo "deb http://repo.kali.org/kali sana main non-free contrib" > /etc/apt/sources.list
+# apt-key adv --keyserver keyserver.ubuntu.com --recv-keys ED444FF07D8D0BF6
+# apt-get update
+# sudo update-alternatives --remove-all gcc 
+# sudo update-alternatives --remove-all g++
+# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
+# sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
+# sudo update-alternatives --config gcc
+# sudo update-alternatives --config g++
 apt-get install libc6-dev-i386 lynx g++-4.8-multilib build-essential libboost-all-dev
 source ~/.bashrc
 url=`lynx -dump -listonly https://software.intel.com/en-us/articles/pintool-downloads | grep -P '\-linux' | cut -d' ' -f3 | sort -nr | head -n 1`
