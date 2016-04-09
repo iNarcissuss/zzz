cbbec08f79bc86c30dfefe4d47825ee581ae8c78
vdun <vdun@users.noreply.github.com>
2016-04-10 00:59:29 +0200

    Update mk.sh

--- build/binnavi/mk.sh
+++ build/binnavi/mk.sh
@@ -2,10 +2,10 @@
 # sudo apt-get update
 # sudo apt-get install openjdk-8-jdk
 
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
+add-apt-repository ppa:webupd8team/java
+apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo apt-get install oracle-java8-installer oracle-java8-set-default
+apt-get install oracle-java8-installer oracle-java8-set-default maven2
 
 mvn dependency:copy-dependencies
 ant build-binnavi-fat-jar
