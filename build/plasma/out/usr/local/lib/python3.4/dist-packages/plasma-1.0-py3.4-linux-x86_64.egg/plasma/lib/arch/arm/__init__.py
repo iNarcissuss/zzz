bf372493f1bf1a3dbfab400cc66d5895225cae98
root <root@dfc2eabdf236>
2016-04-23 12:59:14 +0000

    update

--- build/plasma/out/usr/local/lib/python3.4/dist-packages/plasma-1.0-py3.4-linux-x86_64.egg/plasma/lib/arch/arm/__init__.py
+++
@@ -1,8 +0,0 @@
-import plasma.lib.arch.arm.output
-import plasma.lib.arch.arm.utils
-import plasma.lib.arch.arm.process_ast
-
-registered = [
-    process_ast.convert_cond_to_if,
-    process_ast.fuse_inst_with_if,
-]
