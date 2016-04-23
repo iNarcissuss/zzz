b9dd0c089bbe58e9184ff3c002d5e964dcfab0fe
vdun <vdun@users.noreply.github.com>
2016-04-23 04:15:32 +0200

    Update mk.sh

--- build/simplify/mk.sh
+++ build/simplify/mk.sh
@@ -1,3 +1,6 @@
-apt-get install -y openjdk-7-jdk
+add-apt-repository ppa:webupd8team/java
+apt-get update
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+apt-get install -y oracle-java8-installer oracle-java8-set-default maven2
 git submodule update --init --recursive
 ./gradlew fatjar
