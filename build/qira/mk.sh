2f258905237ee8b449d847efa17ef23eaf3c1100
vdun <vdun@users.noreply.github.com>
2016-04-30 02:11:26 +0200

    Update mk.sh

--- build/qira/mk.sh
+++ build/qira/mk.sh
@@ -1,6 +1,5 @@
 set -x
 sed -i 's|sudo | |gi' ./install.sh
 bash -x ./install.sh
-find ./middleware/* ./static2/* ./qiradb/* | xargs -Ixxx touch xxx
-ls -la
-touch ./*
+find ./* | xargs -Ixxx touch xxx
+# find ./middleware/* ./static2/* ./qiradb/* | xargs -Ixxx touch xxx
