0404d56fc033eb478940ed88738a36ed8717ed6a
vdun <vdun@users.noreply.github.com>
2016-05-03 12:08:18 +0200

    Update mk.sh

--- build/apktool/mk.sh
+++ build/apktool/mk.sh
@@ -2,7 +2,7 @@ apt-get install -y openjdk-7-jdk
 ./gradlew build fatJar
 mkdir ./out
 mv ./build/libs/*.jar ./scripts ./out
-find ./* | grep -v -P '(mk.sh|./out|./scripts)' | xargs -Ixxx rm -rf xxx
-find ./scripts/* | xargs -Ixxx touch xxx
+mv ./scripts ./out
+find ./* | grep -v -P '(mk.sh|./out)' | xargs -Ixxx rm -rf xxx
 mv ./out/* ./
 rm -rf ./out
